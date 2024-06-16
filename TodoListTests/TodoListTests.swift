84365a8e7a9bbd105cf2ccae1f680ba33ea2f067
Krauser Huang <crescentdence@gmail.com>
2024-06-16 16:01:02 +0800

    Initial Commit

---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Tai Chin Huang on 2024/6/16.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
